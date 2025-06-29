7ab55f658684ed9ac4ee462677db384b47db87a2
Nekrofage <lesanglierdesardennes@gmail.com>
2025-06-29 10:13:43 +0200

    [Update] Map01

--- buildGame.sh
+++ buildGame.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+rm obj -rf
+rm src/map/*
 make
--- src/map/mappk0.h
+++ src/map/mappk0.h
@@ -2,22 +2,22 @@
 // Compresor used:   zx7b
 // Files compressed: [ 'src/map0.bin' ]
 // Uncompressed:     1680 bytes
-// Compressed:       634 bytes
-// Space saved:      1046 bytes
+// Compressed:       453 bytes
+// Space saved:      1227 bytes
 //
 
-#ifndef mappk0_634_H
-#define mappk0_634_H
+#ifndef mappk0_453_H
+#define mappk0_453_H
 
 
 // Declaration of the compressed array
-extern const unsigned char mappk0[634];
+extern const unsigned char mappk0[453];
 
 // Address of the latest byte of the compressed array (for unpacking purposes)
-#define mappk0_end      (mappk0 + 634 - 1)
+#define mappk0_end      (mappk0 + 453 - 1)
 
 // Compressed and uncompressed sizes
-#define mappk0_size_z   634
+#define mappk0_size_z   453
 #define mappk0_size     1680
 
 

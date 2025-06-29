dac1163d623be126e93386d429695804a6166456
Nekrofage <lesanglierdesardennes@gmail.com>
2025-06-29 13:47:16 +0200

    [Update] Map

--- src/map/mappk1.h
+++ src/map/mappk1.h
@@ -2,22 +2,22 @@
 // Compresor used:   zx7b
 // Files compressed: [ 'src/map1.bin' ]
 // Uncompressed:     1680 bytes
-// Compressed:       673 bytes
-// Space saved:      1007 bytes
+// Compressed:       549 bytes
+// Space saved:      1131 bytes
 //
 
-#ifndef mappk1_673_H
-#define mappk1_673_H
+#ifndef mappk1_549_H
+#define mappk1_549_H
 
 
 // Declaration of the compressed array
-extern const unsigned char mappk1[673];
+extern const unsigned char mappk1[549];
 
 // Address of the latest byte of the compressed array (for unpacking purposes)
-#define mappk1_end      (mappk1 + 673 - 1)
+#define mappk1_end      (mappk1 + 549 - 1)
 
 // Compressed and uncompressed sizes
-#define mappk1_size_z   673
+#define mappk1_size_z   549
 #define mappk1_size     1680
 
 
--- src/map/mappk2.h
+++ src/map/mappk2.h
@@ -2,22 +2,22 @@
 // Compresor used:   zx7b
 // Files compressed: [ 'src/map2.bin' ]
 // Uncompressed:     1680 bytes
-// Compressed:       501 bytes
-// Space saved:      1179 bytes
+// Compressed:       418 bytes
+// Space saved:      1262 bytes
 //
 
-#ifndef mappk2_501_H
-#define mappk2_501_H
+#ifndef mappk2_418_H
+#define mappk2_418_H
 
 
 // Declaration of the compressed array
-extern const unsigned char mappk2[501];
+extern const unsigned char mappk2[418];
 
 // Address of the latest byte of the compressed array (for unpacking purposes)
-#define mappk2_end      (mappk2 + 501 - 1)
+#define mappk2_end      (mappk2 + 418 - 1)
 
 // Compressed and uncompressed sizes
-#define mappk2_size_z   501
+#define mappk2_size_z   418
 #define mappk2_size     1680
 
 

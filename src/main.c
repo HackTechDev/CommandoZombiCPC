0d5178062eb8dd048ce77c6ca737b5db74207634
Nekrofage <lesanglierdesardennes@gmail.com>
2025-06-29 09:55:36 +0200

    [Add] Logo

--- src/main.c
+++ src/main.c
@@ -1879,33 +1879,11 @@ void EnemyLoop(TSpr *pSpr) __z88dk_fastcall {
 void PrintStartMenu() {
 	ClearScreen();
 
-	
 	cpct_drawSprite(g_logo_0, cpctm_screenPtr(CPCT_VMEM_START, 0, 0), G_LOGO_0_W, G_LOGO_0_H);
 	cpct_drawSprite(g_logo_1, cpctm_screenPtr(CPCT_VMEM_START, G_LOGO_0_W, 0), G_LOGO_0_W, G_LOGO_0_H);
-	
 
-	/*
-	cpct_drawSprite(g_logo_00, cpctm_screenPtr(CPCT_VMEM_START, 0, 0), G_LOGO_00_W, G_LOGO_00_H);
-	cpct_drawSprite(g_logo_01, cpctm_screenPtr(CPCT_VMEM_START, G_LOGO_00_W, 0), G_LOGO_00_W, G_LOGO_00_H);
-  
-	cpct_drawSprite(g_logo_02, cpctm_screenPtr(CPCT_VMEM_START, 0, G_LOGO_00_H), G_LOGO_00_W, G_LOGO_00_H);
-	cpct_drawSprite(g_logo_03, cpctm_screenPtr(CPCT_VMEM_START, G_LOGO_00_W, G_LOGO_00_H), G_LOGO_00_W, G_LOGO_00_H);
-  
-	cpct_drawSprite(g_logo_04, cpctm_screenPtr(CPCT_VMEM_START, 0, G_LOGO_00_H * 2), G_LOGO_00_W, G_LOGO_00_H);
-	cpct_drawSprite(g_logo_05, cpctm_screenPtr(CPCT_VMEM_START, G_LOGO_00_W, G_LOGO_00_H * 2), G_LOGO_00_W, G_LOGO_00_H);
-  
-	cpct_drawSprite(g_logo_06, cpctm_screenPtr(CPCT_VMEM_START, 0, G_LOGO_00_H * 3), G_LOGO_00_W, G_LOGO_00_H);
-	cpct_drawSprite(g_logo_07, cpctm_screenPtr(CPCT_VMEM_START, G_LOGO_00_W, G_LOGO_00_H * 3), G_LOGO_00_W, G_LOGO_00_H);
-  
-	cpct_drawSprite(g_logo_08, cpctm_screenPtr(CPCT_VMEM_START, 0, G_LOGO_00_H * 4), G_LOGO_00_W, G_LOGO_00_H);
-	cpct_drawSprite(g_logo_09, cpctm_screenPtr(CPCT_VMEM_START, G_LOGO_00_W, G_LOGO_00_H * 4), G_LOGO_00_W, G_LOGO_00_H);
-  
-	cpct_drawSprite(g_logo_10, cpctm_screenPtr(CPCT_VMEM_START, 0, G_LOGO_00_H * 5), G_LOGO_10_W, G_LOGO_10_H);
-	cpct_drawSprite(g_logo_11, cpctm_screenPtr(CPCT_VMEM_START, G_LOGO_00_W, G_LOGO_00_H * 5), G_LOGO_11_W, G_LOGO_11_H);
-	*/
-
-	PrintText("COMMANDO", 5, 10, 0);
-	cpct_drawSpriteMaskedAlignedTable(g_sorcerer1_03, cpct_getScreenPtr(CPCT_VMEM_START, 5, 30), SPR_W, SPR_H, g_maskTable);
+	PrintText("COMMANDO", 15, 42, 0);
+	cpct_drawSpriteMaskedAlignedTable(g_sorcerer1_03, cpct_getScreenPtr(CPCT_VMEM_START, 5, 40), SPR_W, SPR_H, g_maskTable);
 }
 
 
